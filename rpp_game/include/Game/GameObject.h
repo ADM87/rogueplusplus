44fe4ad8b10ef9235e5bfe9a54b4266191a9321d
ADM87 <aaron.d.malley@gmail.com>
2023-11-22 20:26:31 -0400

    Moved world back to rpp_game

--- rpp_game/include/Game/GameObject.h
+++ rpp_game/include/Game/GameObject.h
@@ -15,6 +15,7 @@ namespace rpp
         GameObject(const std::string& _name)
             : Entity(_name, { "Transform", "Renderer" })
             , m_transform(nullptr)
+            , m_renderer(nullptr)
         {}
 
         TransformComponent* const Transform();
